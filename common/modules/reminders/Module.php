175d582d9d8c2ab0e8e539b3826993907a840e1b
yaKostin <yaKostin@github.com>
2015-06-08 00:12:49 +0300

    Testing git features

---
+++ common/modules/reminders/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace common\modules\reminders;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'common\modules\reminders\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
